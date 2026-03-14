2e2a108b56e91617c3c4855f5df640680bd1346f
PaulaStaab <paula.staab@web.de>
2026-03-14 16:58:48 +0100

    ADD: TMP, ADD: Startscreen, UPD: Monster Movement

--- Assets/Scripts/MonsterEnemy.cs
+++ Assets/Scripts/MonsterEnemy.cs
@@ -2,28 +2,37 @@ using UnityEngine;
 
 public class MonsterEnemy : MonoBehaviour
 {
-    [Header("Bewegung")]
+    [Header("Movement")]
     public float speed = 3f;
     public float stopDistance = 1.5f;
 
     [Header("Prefabs")]
-    public GameObject monsterPrefab; // Monster-Prefab
+    public GameObject monsterPrefab; // Monster-Prefab zum Instanziieren
 
+    [Header("Patrol Points")]
+    public Transform[] waypoints; // Weise Waypoints im Inspector zu
+    private int currentWaypointIndex = 0;
 
     void Update()
     {
-        if (monsterPrefab == null || playerTarget == null)
+        if (monsterPrefab == null || waypoints == null || waypoints.Length == 0)
             return;
 
-        float dist = Vector2.Distance(transform.position, playerTarget.position);
+        Transform target = waypoints[currentWaypointIndex];
+        float dist = Vector2.Distance(transform.position, target.position);
 
         if (dist > stopDistance)
         {
             transform.position = Vector2.MoveTowards(
                 transform.position,
-                playerTarget.position,
+                target.position,
                 speed * Time.deltaTime
             );
+        }
+        else
+        {
+            // Nächsten Waypoint wählen
+            currentWaypointIndex = (currentWaypointIndex + 1 ) % waypoints.Length;
         }
     }
 }
