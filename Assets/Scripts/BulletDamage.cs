7081ff7bc406636a6558dd833f6e8ae2df3b8cbb
PaulaStaab <paula.staab@web.de>
2026-04-18 21:49:57 +0200

    UPD: Tower3

--- Assets/Scripts/BulletDamage.cs
+++ Assets/Scripts/BulletDamage.cs
@@ -1,10 +1,59 @@
+//////////using UnityEngine;
+
+//////////public class BulletDamage : MonoBehaviour
+//////////{
+//////////    public float damage = 100f;
+//////////    public float speed = 20f;
+//////////    public GameObject explosionPrefab;  // Ziehe deinen Explosion-Prefab hier rein
+
+//////////    void Update()
+//////////    {
+//////////        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+//////////    }
+
+//////////    private void OnTriggerEnter(Collider other)
+//////////    {
+//////////        if (other.CompareTag("Enemy"))
+//////////        {
+//////////            SpiderHealth enemyHealth = other.GetComponent<SpiderHealth>();
+//////////            if (enemyHealth != null)
+//////////            {
+//////////                float healthBefore = enemyHealth.currentHealth;  // Aktuelle Health merken (muss public sein)
+//////////                enemyHealth.TakeDamage(damage);
+
+//////////                // Prüfen, ob Spinne durch diesen Schuss gestorben ist
+//////////                if (healthBefore > 0 && enemyHealth.currentHealth <= 0)
+//////////                {
+//////////                    SpawnExplosion(other.transform.position);
+//////////                }
+//////////            }
+//////////            else
+//////////            {
+//////////                Destroy(other.gameObject);
+//////////                SpawnExplosion(other.transform.position);
+//////////            }
+//////////        }
+
+//////////        Destroy(gameObject);
+//////////    }
+
+//////////    void SpawnExplosion(Vector3 position)
+//////////    {
+//////////        if (explosionPrefab != null)
+//////////        {
+//////////            GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+//////////            // Optional: Zerstöre nach Dauer (falls nicht im Prefab-Skript)
+//////////            Destroy(explosion, 2f);
+//////////        }
+//////////    }
+//////////}
+
 ////////using UnityEngine;
 
 ////////public class BulletDamage : MonoBehaviour
 ////////{
-////////    public float damage = 100f;
 ////////    public float speed = 20f;
-////////    public GameObject explosionPrefab;  // Ziehe deinen Explosion-Prefab hier rein
+////////    public GameObject explosionPrefab;
 
 ////////    void Update()
 ////////    {
@@ -15,23 +64,8 @@
 ////////    {
 ////////        if (other.CompareTag("Enemy"))
 ////////        {
-////////            SpiderHealth enemyHealth = other.GetComponent<SpiderHealth>();
-////////            if (enemyHealth != null)
-////////            {
-////////                float healthBefore = enemyHealth.currentHealth;  // Aktuelle Health merken (muss public sein)
-////////                enemyHealth.TakeDamage(damage);
-
-////////                // Prüfen, ob Spinne durch diesen Schuss gestorben ist
-////////                if (healthBefore > 0 && enemyHealth.currentHealth <= 0)
-////////                {
-////////                    SpawnExplosion(other.transform.position);
-////////                }
-////////            }
-////////            else
-////////            {
-////////                Destroy(other.gameObject);
-////////                SpawnExplosion(other.transform.position);
-////////            }
+////////            SpawnExplosion(other.transform.position);
+////////            Destroy(other.gameObject);
 ////////        }
 
 ////////        Destroy(gameObject);
@@ -42,16 +76,17 @@
 ////////        if (explosionPrefab != null)
 ////////        {
 ////////            GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
-////////            // Optional: Zerstöre nach Dauer (falls nicht im Prefab-Skript)
 ////////            Destroy(explosion, 2f);
 ////////        }
 ////////    }
 ////////}
 
+//////using System;
 //////using UnityEngine;
 
 //////public class BulletDamage : MonoBehaviour
 //////{
+//////    public float damage = 100f;
 //////    public float speed = 20f;
 //////    public GameObject explosionPrefab;
 
@@ -64,8 +99,31 @@
 //////    {
 //////        if (other.CompareTag("Enemy"))
 //////        {
-//////            SpawnExplosion(other.transform.position);
-//////            Destroy(other.gameObject);
+//////            bool enemyDied = false;
+
+//////            // Beispiel: Enemy hat SpiderHealth
+//////            EnemyController enemyHealth = other.GetComponent<EnemyController>();
+//////            if (enemyHealth != null)
+//////            {
+//////                float healthBefore = enemyHealth.currentHealth;
+//////                enemyHealth.TakeDamage(damage);
+
+//////                if (healthBefore > 0 && enemyHealth.currentHealth <= 0)
+//////                {
+//////                    enemyDied = true;
+//////                }
+//////            }
+//////            else
+//////            {
+//////                // Falls kein Health-Skript gefunden wurde, optional direkt zerstören
+//////                Destroy(other.gameObject);
+//////                enemyDied = true;
+//////            }
+
+//////            if (enemyDied)
+//////            {
+//////                SpawnExplosion(other.transform.position);
+//////            }
 //////        }
 
 //////        Destroy(gameObject);
@@ -78,6 +136,11 @@
 //////            GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
 //////            Destroy(explosion, 2f);
 //////        }
+//////    }
+
+//////    internal int GetDamage()
+//////    {
+//////        throw new NotImplementedException();
 //////    }
 //////}
 
@@ -140,7 +203,7 @@
 
 ////    internal int GetDamage()
 ////    {
-////        throw new NotImplementedException();
+////        return Mathf.RoundToInt(damage);
 ////    }
 ////}
 
@@ -164,21 +227,13 @@
 //        {
 //            bool enemyDied = false;
 
-//            // Beispiel: Enemy hat SpiderHealth
 //            EnemyController enemyHealth = other.GetComponent<EnemyController>();
 //            if (enemyHealth != null)
 //            {
-//                float healthBefore = enemyHealth.currentHealth;
-//                enemyHealth.TakeDamage(damage);
-
-//                if (healthBefore > 0 && enemyHealth.currentHealth <= 0)
-//                {
-//                    enemyDied = true;
-//                }
+//                enemyDied = enemyHealth.TakeDamage(Mathf.RoundToInt(damage));
 //            }
 //            else
 //            {
-//                // Falls kein Health-Skript gefunden wurde, optional direkt zerstören
 //                Destroy(other.gameObject);
 //                enemyDied = true;
 //            }
@@ -212,11 +267,28 @@ using UnityEngine;
 
 public class BulletDamage : MonoBehaviour
 {
+    [Header("Stats")]
     public float damage = 100f;
     public float speed = 20f;
-    public GameObject explosionPrefab;
 
-    void Update()
+    [Header("Effects")]
+    public GameObject explosionPrefab;
+    public bool useTrail = false;
+
+    private TrailRenderer trail;
+
+    private void Awake()
+    {
+        trail = GetComponent<TrailRenderer>();
+
+        if (trail != null)
+        {
+            trail.enabled = useTrail;
+            trail.Clear();
+        }
+    }
+
+    private void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
@@ -247,7 +319,7 @@ public class BulletDamage : MonoBehaviour
         Destroy(gameObject);
     }
 
-    void SpawnExplosion(Vector3 position)
+    private void SpawnExplosion(Vector3 position)
     {
         if (explosionPrefab != null)
         {
